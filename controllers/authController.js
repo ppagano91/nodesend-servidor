f6d9f764be9c0e3a7dd48f1c4ebff8bcc0db817a
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-24 16:18:11 -0300

    Agregando Express Validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,17 @@
 const Usuario = require("../models/Usuario");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
 
 require("dotenv").config({ path: ".env" });
 
 exports.autenticarUsuario = async (req, res, next) => {
   // Revisar si hay errores.
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
   // Buscar el usuario para ver si está registrado.
   const { email, password } = req.body;
 
--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,16 @@ const authController = require("../controllers/authController");
 const { check } = require("express-validator");
 const { route } = require("./usuarios");
 
-router.post("/", authController.autenticarUsuario);
+router.post(
+  "/",
+  [
+    check("email", "Agrega un email válido.").isEmail(),
+    check("password", "El password debe tener al menos 6 caracteres.").isLength(
+      { min: 6 }
+    ),
+  ],
+  authController.autenticarUsuario
+);
 
 router.get("/", authController.usuarioAutenticado);
 
