d0f4fd308aacaedc9fcd2d8578bbfa97b4dc9979
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-24 16:55:08 -0300

    Guardando enlaces en la BD

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -1,3 +1,26 @@
+const Enlaces = require("../models/Enlace");
+const shortid = require("shortid");
+
 exports.nuevoEnlace = async (req, res, next) => {
-  console.log("Nuevo enlace");
+  // Revisar si hay errores.
+
+  // Almacenar en la base de datos.
+  const { nombre_original, password } = req.body;
+
+  // Crear Objeto Enlace
+  const enlace = new Enlaces();
+  enlace.url = shortid.generate();
+  enlace.nombre = shortid.generate();
+  enlace.nombre_original = nombre_original;
+  enlace.password = password;
+
+  //   Almacenar en la base de datos.
+  try {
+    await enlace.save();
+    res.json({ msg: `${enlace.url}` });
+  } catch (error) {
+    console.log(error);
+  }
+
+  // Si el usuario está autenticado.
 };
