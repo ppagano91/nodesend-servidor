a281bd9b2be8104d9bd2c0c2f1ae3c4c6fdb7d2f
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-24 16:22:56 -0300

    Obtener el Usuario Autenticado por el JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,4 +44,20 @@ exports.autenticarUsuario = async (req, res, next) => {
   }
 };
 
-exports.usuarioAutenticado = async (req, res, next) => {};
+exports.usuarioAutenticado = async (req, res, next) => {
+  const authHeader = req.get("Authorization");
+
+  if (authHeader) {
+    // Obtener el token.
+    const token = authHeader.split(" ")[1];
+
+    // Comprobar el JWT.
+    try {
+      const usuario = jwt.verify(token, process.env.SECRETA);
+      res.json({ usuario });
+    } catch (error) {
+      console.log(error);
+      res.status(401).json({ msg: "Token no válido." });
+    }
+  }
+};
