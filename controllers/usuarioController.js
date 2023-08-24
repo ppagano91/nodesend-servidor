e855796d0dfc701addd3b6144fac09f72e7df607
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-24 12:27:39 -0300

    Evitar usuarios duplicados + Hash de password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,8 +1,29 @@
 const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcrypt");
 
 exports.nuevoUsuario = async (req, res) => {
-  const usuario = await new Usuario(req.body);
-  usuario.save();
+  // Verificar si el usuario ya está registrado.
+  const { email, password } = req.body;
 
-  res.json({ msg: "Usuario creado correctamente." });
+  let usuario = await Usuario.findOne({ email });
+
+  if (usuario) {
+    return res.status(400).json({ msg: "El usuario ya está registrado." });
+  }
+
+  // Crear un nuevo usuario.
+  usuario = new Usuario(req.body);
+
+  // Hashear el password.
+  const salt = await bcryptjs.genSalt(10);
+  usuario.password = await bcryptjs.hash(password, salt);
+
+  try {
+    // Guardar el usuario.
+    await usuario.save();
+    res.json({ msg: "Usuario creado correctamente." });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "Hubo un error al crear el Usuario" });
+  }
 };
