10cfb7cbf555f308990ac196489e5988b40d9f18
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-24 16:15:23 -0300

    Creando un JSON Web Token (JWT)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 const Usuario = require("../models/Usuario");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config({ path: ".env" });
 
 exports.autenticarUsuario = async (req, res, next) => {
   // Revisar si hay errores.
@@ -16,7 +19,19 @@ exports.autenticarUsuario = async (req, res, next) => {
 
   // Verificar el password y autenticar el usuario.
   if (bcrypt.compareSync(password, usuario.password)) {
-    res.status(200).json({ msg: "El usuario se autenticó correctamente." });
+    const token = jwt.sign(
+      {
+        id: usuario._id,
+        nombre: usuario.nombre,
+        email: usuario.email,
+      },
+      process.env.SECRETA,
+      {
+        expiresIn: "8h",
+      }
+    );
+
+    res.json({ token });
   } else {
     res.status(401).json({ msg: "Password incorrecto." });
     return next();
