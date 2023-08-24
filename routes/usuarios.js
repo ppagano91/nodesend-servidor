067ef53ae720b6e8ea4f5380d440fbc82a30243e
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-24 12:35:07 -0300

    Añadiendo Express Validator

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,12 +1,19 @@
 const express = require("express");
-
 const router = express.Router();
-
 const usuarioController = require("../controllers/usuarioController");
+const { check } = require("express-validator");
 
 // Crea un usuario.
-router.post("/", (req, res) => {
-  usuarioController.nuevoUsuario(req, res);
-});
+router.post(
+  "/",
+  [
+    check("nombre", "El nombre es obligatorio.").not().isEmpty(),
+    check("email", "Agrega un email válido.").isEmail(),
+    check("password", "El password debe tener al menos 6 caracteres.").isLength(
+      { min: 6 }
+    ),
+  ],
+  usuarioController.nuevoUsuario
+);
 
 module.exports = router;
