f5f75c9c0cce73bcaba3f8b10d29df5937ec5bdd
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-24 16:06:04 -0300

    Verificando si un usuario existe

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,22 @@
-exports.autenticarUsuario = async (req, res, next) => {};
+const Usuario = require("../models/Usuario");
+
+exports.autenticarUsuario = async (req, res, next) => {
+  // Revisar si hay errores.
+  // Buscar el usuario para ver si está registrado.
+  const { email, password } = req.body;
+
+  const usuario = await Usuario.findOne({ email });
+  console.log(usuario);
+
+  if (!usuario) {
+    res.status(401).json({ msg: "El usuario no existe." });
+    return next();
+  }
+
+  // Verificar el password y autenticar el usuario.
+  if (password === usuario.password) {
+    res.status(200).json({ msg: "El usuario se autenticó correctamente." });
+  }
+};
 
 exports.usuarioAutenticado = async (req, res, next) => {};
