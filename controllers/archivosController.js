2c7b3a5dd27661781925077e0dd98e28c2bffdde
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-25 12:27:41 -0300

    Renombrando los Archivos y añadiendo extensión

--- controllers/archivosController.js
+++ controllers/archivosController.js
@@ -1,36 +1,32 @@
 const multer = require("multer");
 const shortid = require("shortid");
 
-const configuracionMulter = {
-  limits: { fileSize: 1000000 },
-  storage: (fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, __dirname + "/../uploads");
-    },
-    filename: (req, file, cb) => {
-      const extension = file.mimetype.split("/")[1];
-      cb(null, `${shortid.generate()}.${extension}`);
-    },
-  })),
-};
-
-const upload = multer(configuracionMulter).single("archivo");
-
 exports.subirArchivo = async (req, res, next) => {
-  try {
-    upload(req, res, async (error) => {
-      if (!error) {
-        res.json({ archivo: req.file.filename });
-      } else {
-        console.log("error", error);
-        return next();
-      }
-    });
-  } catch (error) {
-    console.log(error);
-    return next();
-  }
-  //
+  const configuracionMulter = {
+    limits: { fileSize: req.usuario ? 1024 * 1024 * 10 : 1024 * 1024 },
+    storage: (fileStorage = multer.diskStorage({
+      destination: (req, file, cb) => {
+        cb(null, __dirname + "/../uploads");
+      },
+      filename: (req, file, cb) => {
+        const extension = file.originalname.substring(
+          file.originalname.lastIndexOf("."),
+          file.originalname.length
+        );
+        cb(null, `${shortid.generate()}${extension}`);
+      },
+    })),
+  };
+
+  const upload = multer(configuracionMulter).single("archivo");
+  upload(req, res, async (error) => {
+    if (!error) {
+      res.json({ archivo: req.file.filename });
+    } else {
+      console.log("error", error);
+      return next();
+    }
+  });
 
   //   console.log(req.file);
   //   const { archivo } = req.files;
--- routes/archivos.js
+++ routes/archivos.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const archivosController = require("../controllers/archivosController");
 const auth = require("../middleware/auth");
 
-router.post("/", archivosController.subirArchivo);
+router.post("/", auth, archivosController.subirArchivo);
 
 router.delete("/:id", archivosController.eliminarArchivo);
 
